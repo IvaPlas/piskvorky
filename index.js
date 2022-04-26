bfa2c175208358b607c68ef483dc536d732b0c69
Ivana Plašienková <ivana.plasienkova@gmail.com>
2022-04-26 13:25:46 +0200

    Gamer pictogram changed when on turn, added another possibility of animation.

--- index.js
+++ index.js
@@ -2,25 +2,18 @@ console.log('frcim');
 
 let naTahuJe = 'circle';
 const addClass = (ev) => {
-  // ev = click event
-  // target - identifies which button was clicked
-  // classList.add adds cross / circle
-  if (ev.target.disabled === true) {
-    return null;
-  }
   console.log('Na tahu je : ', naTahuJe);
   if (naTahuJe === 'circle') {
     ev.target.classList.add('playground_button--circle');
-    document.querySelector('#pictogram').src = 'images/circle.svg';
+    document.querySelector('#pictogram').src = 'images/cross.svg';
     naTahuJe = 'cross';
     ev.target.disabled = true;
   } else if (naTahuJe === 'cross') {
     ev.target.classList.add('playground_button--cross');
-    document.querySelector('#pictogram').src = 'images/cross.svg';
+    document.querySelector('#pictogram').src = 'images/circle.svg';
     naTahuJe = 'circle';
     ev.target.disabled = true;
   }
-  console.log(ev.target.disabled);
 };
 
 const buttons = document.querySelectorAll('.playground_button');
